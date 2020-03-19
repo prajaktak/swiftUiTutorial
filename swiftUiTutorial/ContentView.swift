3a0d3181d35d227c3f3f36f8eba4a46759f2b6de
Prajakta <prachee.j@gmail.com>
2020-03-19 01:02:31 +0100

    Initial Commit

---
+++ swiftUiTutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUiTutorial
+//
+//  Created by Prajakta Kulkarni on 19/03/2020.
+//  Copyright © 2020 Prajakta Kulkarni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
