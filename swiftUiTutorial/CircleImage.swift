4799c76387696f3d6069478ba924631fb0635303
Prajakta <prachee.j@gmail.com>
2020-03-23 23:58:47 +0100

    added list view and navigation using swiftUI

---
+++ swiftUiTutorial/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  swiftUiTutorial
+//
+//  Created by Prajakta Kulkarni on 22/03/2020.
+//  Copyright © 2020 Prajakta Kulkarni. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+    
+    var body: some View {
+        image
+        .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
---
+++ swiftUiTutorial/LandmarkDetail.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkDetail.swift
+//  swiftUiTutorial
+//
+//  Created by Prajakta Kulkarni on 19/03/2020.
+//  Copyright © 2020 Prajakta Kulkarni. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var landmark:Landmark
+    
+    var body: some View {
+        VStack {
+            MapView(coordinate: landmark.locationCoordinate)
+                .edgesIgnoringSafeArea(.top)
+                .frame(height:300)
+            CircleImage(image: landmark.image)
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                HStack {
+                    Text(landmark.park)
+                                   .font(.subheadline)
+                    Spacer()
+                    Text(landmark.state)
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            Spacer()
+        }
+         .navigationBarTitle(Text(landmark.name), displayMode: .inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarkData[0])
+    }
+}
