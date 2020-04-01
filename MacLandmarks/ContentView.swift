0ea4532071487f45d754c83ead4c79ff72c06c7f
Prajakta <prachee.j@gmail.com>
2020-04-01 22:10:01 +0200

    created mac OS target

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Prajakta Kulkarni on 01/04/2020.
+//  Copyright © 2020 Prajakta Kulkarni. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
