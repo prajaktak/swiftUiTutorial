5dc7f33fcd901cbce95867e4328deefbf700e783
Prajakta <prachee.j@gmail.com>
2020-04-02 12:15:38 +0200

    changed circleImage and map view to go with macOS app

--- swiftUiTutorial/SupportingViews/MapView.swift
+++ swiftUiTutorial/SupportingViews/MapView.swift
@@ -9,23 +9,47 @@
 import SwiftUI
 import MapKit
 
-struct MapView: UIViewRepresentable {
+struct MapView {
     var coordinate: CLLocationCoordinate2D
     
-    func makeUIView(context: Context) -> MKMapView {
+    func makeMapView() -> MKMapView {
         MKMapView(frame: .zero)
     }
     
-    func updateUIView(_ uiView: MKMapView, context: Context) {
+    func updateMapView(_ view: MKMapView, context: Context) {
         let coordinate = CLLocationCoordinate2D(
             latitude: 34.011286, longitude: -116.166868)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
-        uiView.setRegion(region, animated: true)
+        view.setRegion(region, animated: true)
     }
 
 }
+#if os(macOS)
 
+extension MapView: NSViewRepresentable {
+    func makeNSView(context: Context) -> MKMapView {
+        makeMapView()
+    }
+    
+    func updateNSView(_ nsView: MKMapView, context: Context) {
+        updateMapView(nsView, context: context)
+    }
+}
+
+#else
+
+extension MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        makeMapView()
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        updateMapView(uiView, context: context)
+    }
+}
+
+#endif
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView(coordinate: landmarkData[0].locationCoordinate)
